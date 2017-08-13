3862f0cfb6139a695f5d3b45c9ad977fbc1e1143
wahula4 <mawahula4@gmail.com>
2017-08-13 14:41:18 -0600

    friend finder

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,21 @@
 
+var friendArray = [
+  {
+  "friendName":"Tony",
+  "friendURL":"https://s-media-cache-ak0.pinimg.com/736x/f5/1d/08/f51d08be05919290355ac004cdd5c2d6--pikachu-tattoo-pikachu-drawing.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+]
+
+module.exports = friendArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,51 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to "data"
+// ===============================================================================
+var friendData = require("../data/friends");
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(friendData);
+    });
 
+    app.post("/api/friends", function(req, res) {
+        var difference = 1000; 
+        var matchName = "";
+        var matchPhoto = "";
+
+        var newFriend = req.body;
+
+        for (var i = 0; i < friendData.length; i++) {
+            var matchScores = [];
+            var totalDifference = 0;
+
+            for (var j = 0; j < 10; j++) {
+                matchScores.push(
+                    Math.abs(
+                        parseInt(newFriend.scores[j]) -
+                            parseInt(friendData[i].scores[j])
+                    )
+                );
+
+                totalDifference = matchScores.reduce(function(total, amount) {
+                    return total + amount;
+                });
+            }
+            console.log(friendData[i].friendName + ": " + totalDifference);
+
+            if (totalDifference < difference) {
+                difference = totalDifference;
+                matchName = friendData[i].friendName;
+                matchPhoto = friendData[i].friendURL;
+            }
+        }
+        //Once the cycle is complete, the friend with the least difference will remain, and that data will be sent as a json object back to the client
+        res.json({ friendName: matchName, friendURL: matchPhoto });
+
+        //Lastly, push new User's data into the friends array
+        friendData.push(newFriend);
+    });
+};
